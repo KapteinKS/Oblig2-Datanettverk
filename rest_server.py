407b921772a500ca8df8433ba815c8e0476fd7d6
KapteinKS <sander.toien@outlook.com>
2021-04-07 11:27:46 +0200

    Update rest_server.py
    skeleton code

--- rest_server.py
+++ rest_server.py
@@ -4,5 +4,20 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+users = []
+
+
+class Users(Resource):
+    def get(self): # return users
+    
+    def post(self): # users.add
+    
+class User(Resource):
+    def get(self): # return user
+    
+    def post(self): # delete
+        
+class Rooms(Resource):
+        
 if __name__ == "__main__":
     app.run(debug=True)
