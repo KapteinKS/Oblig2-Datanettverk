0d9d76a1ad28dac0a05922230bc9f3465e7af027
KapteinKS <sander.toien@outlook.com>
2021-04-07 11:33:27 +0200

    Update rest_server.py
    finished skeleton code

--- rest_server.py
+++ rest_server.py
@@ -18,6 +18,25 @@ class User(Resource):
     def post(self): # delete
         
 class Rooms(Resource):
+    def get(self): # get all
+        
+    def post(self): # add one
+        
+class Room(Resource):
+    def get(self): # get room
+        
+class RoomUsers(Resource):
+    def get(self): # get all
+    
+    def post(self): # add one
+        
+class Messages(Resource):
+    def get(self): # get all
+        
+class RoomUserMessages(Resource):
+    def get(self): # get all
+        
+    def post(self): # add one
         
 if __name__ == "__main__":
     app.run(debug=True)
