eb4db4d80d7c405916ee0b7bb00562f458e00580
Henrik Stangeland <henrik.b.stangeland@gmail.com>
2021-04-07 11:26:42 +0200

    teststeatst

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+array = {"Joe", "Bob", "Briggs"}
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/api/listAll')
+def listAll():
+    out = ""
+    for name in array:
+        out += name + " & "
+    return out 
