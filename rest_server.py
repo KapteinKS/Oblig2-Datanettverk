95612948ac936402b744d54cc657e4231d9c5373
KapteinKS <sander.toien@outlook.com>
2021-04-07 11:11:29 +0200

    Update rest_server.py

--- rest_server.py
+++ rest_server.py
@@ -4,20 +4,5 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-
-fem = 5
-
-
-def die(you):
-    print(you, "is dead")
-
-
-def start():
-    die("Sander")
-
-
-start()
-
-if __name__ == "__rest_server__":
+if __name__ == "__main__":
     app.run(debug=True)
-    
