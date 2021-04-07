183190ca8b56971b938b410b3797d0fef965bf04
KapteinKS <sander.toien@outlook.com>
2021-04-07 11:41:42 +0200

    Update rest_server.py
    actually finished skeleton

--- rest_server.py
+++ rest_server.py
@@ -38,5 +38,13 @@ class RoomUserMessages(Resource):
         
     def post(self): # add one
         
+api.add_resource(Users, "/api/users")
+api.add_resource(User, "/api/user/<user-id>")
+api.add_resource(Rooms, "/api/rooms")
+api.add_resource(Room, "/api/room/<room-id")
+api.add_resource(RoomUsers, "/api/room/<room-id>/users")
+api.add_resource(Messages, "/api/room/<room-id>/messages")
+api.add_resource(RoomUserMessages, "/api/room/<room-id/<user-id/messages")
+
 if __name__ == "__main__":
     app.run(debug=True)
