1d1b84e89b050cc3801134c25850a15a5f518917
Henrik Stangeland <henrik.b.stangeland@gmail.com>
2021-04-09 13:53:06 +0200

    Aaaaa

--- rest_server.py
+++ rest_server.py
@@ -95,8 +95,8 @@ class RoomUsers(Resource):
     def get(self, room_id):  # get all user in a room by room ID
         # TODO get users from list, return JSON
         if room_id in rooms:
-            #out = rooms[room_id]
-            return str(rooms[room_id])
+            out = json.loads(str(rooms[room_id]))
+            return out.listOfUsers
             #return rooms[room_id].users  # I guess
         else:
             abort(404, message="No room found with that ID")
