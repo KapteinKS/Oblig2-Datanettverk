e57efbfc920d35a38e7f7f0f333ef906d19f97e6
Kristian Wang <krkwang@hotmail.com>
2021-04-09 13:35:59 +0200

    Aksesser server fra client terminal test

--- client.py
+++ client.py
@@ -2,5 +2,14 @@ import requests
 # TODO everything
 BASE = "http://127.0.0.1:5000/api/"
 
-response = requests.get(BASE + "users")
-print(response.json())
+
+def get_users():
+
+    response = requests.get(BASE + "users")
+    print(response.json())
+
+
+while(True):
+    text = input()
+    if text == "/users":
+        get_users()
